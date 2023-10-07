9f7fc844857b1abbbf37340a0f3506f7e2dddcc7
Purice Victor <pvictor0110@gmail.com>
2023-10-07 17:40:29 +0300

    initialized the project

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import React,{useState, useEffect} from 'react';
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((res) => setData(res.message));
+  }, []);
+
+  return (
+    <div className="App">
+        <h1>
+          {!data ? "Loading..." : data}
+        </h1>
+    </div>
+  );
+}
+
+export default App;
