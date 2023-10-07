915f55a9bfaf94590fa4b9ddb34cfb5984cd44ce
Purice Victor <pvictor0110@gmail.com>
2023-10-07 18:49:42 +0300

    implemented frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,37 @@ import React,{useState, useEffect} from 'react';
 function App() {
   const [data, setData] = useState(null);
 
-  useEffect(() => {
+  useEffect(()=>{
     fetch("/api")
-      .then((res) => res.json())
-      .then((res) => setData(res.message));
-  }, []);
+    .then((res)=>res.json())
+    .then((data)=>setData(data.message))
+  },[])
 
   return (
     <div className="App">
-        <h1>
-          {!data ? "Loading..." : data}
-        </h1>
+        {/* <p>
+          {data ? data : "Loading..."}
+        </p> */}
+        <div class="login-box">
+          <h2>Type your ticket</h2>
+            <form>
+              <div class="user-box">
+                <input type="text" name="" required=""/>
+                <label>Full name</label>
+              </div>
+              <div class="user-box">
+                <input type="password" name="" required=""/>
+                <label>Message</label>
+              </div>
+              <a href="#">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                send
+              </a>
+            </form>
+        </div>
     </div>
   );
 }
