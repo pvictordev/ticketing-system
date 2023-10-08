588937b38651a213b0b2df0b647f4b27bc6545da
Purice Victor <pvictor0110@gmail.com>
2023-10-08 17:26:47 +0300

    added database and set data

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 // Main entry point for the backend
 const express = require('express');
+// const cors = require('cors');
+// const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3001;
 
@@ -9,8 +12,10 @@ app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
 
+app.use(bodyParser.json());
+
 app.get('/api', (req, res) => {
     res.json({
-        message: 'Hello from server!' 
+        message: 'Hello from server!',
     });
 });
--- frontend/src/pages/Ticket.js
+++ frontend/src/pages/Ticket.js
@@ -2,21 +2,35 @@ import React,{useState} from 'react';
 import {IoCheckmarkDoneCircle} from 'react-icons/io5';
 import {AiFillCloseCircle} from 'react-icons/ai';
 import { Link } from "react-router-dom";
-import { set } from 'mongoose';
+import axios from 'axios';
 
 
 const Ticket = () => {
-    const [formData, setFormData] = useState({fullName:'', message:''});
+  // const [formData, setFormData] = useState({ fullName: '', message: '' });
+  const[fullName,setFullName]=useState('');
+  const[message,setMessage]=useState('');
 
-    const handleInput = (e) => {
-        const {name, value} = e.target;
-        setFormData({...formData, [name]:value})
-    }
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log(fullName);
+  //   console.log(message);
+  //   axios.post('http://localhost:3001/api/ticket', {
+  //     fullName: fullName,
+  //     message: message
+  //   })
+  //   .then(function (response) {
+  //     console.log(response);
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //   });
+  // };
 
-    const handleFormSubmit = (e) => {
-        e.preventDefault();
-        console.log(formData)
-    }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(fullName +' '+ message);
+  }
+
 
     return (
         <>
@@ -26,28 +40,27 @@ const Ticket = () => {
         <div className="login-box">
    
           <h2>Type your ticket</h2>
-            <form onSubmit={handleFormSubmit}>
+            <form onSubmit={handleSubmit}>
               <div className="user-box">
-                <input type="text" name="" required=""
-                onChange={handleInput}
-                value={formData.fullName}
+                <input type="text" required=""
                 name='fullName'
+                value={fullName}
+                onChange={(e)=>setFullName(e.target.value)}
                 />
                 <label>Full name</label>
               </div>
               <div className="user-box">
-                <input type="text" name="" required=""
-                onChange={handleInput}
-                value={formData.message} 
-                name='message'
+                <input type="text" required=""
+                  name='message'
+                  value={message}
+                  onChange={(e)=>setMessage(e.target.value)}
                 />
            
                 <label>Message</label>
               </div>
+              {/* {nameError && <div className="error">{nameError}</div>} */}
               <div className='result-box'>
-                <button onClick={() => {
-
-                }}>
+                <button type='submit'>
                   <span></span>
                   <span></span>
                   <span></span>
