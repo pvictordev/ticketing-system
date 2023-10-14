7eac6ed6758001f96de925c74d6a2799b0ef8130
Purice Victor <pvictor0110@gmail.com>
2023-10-14 20:49:11 +0300

    commit

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,61 +1,62 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import {MdOutlineSmsFailed, MdOutlineTextsms} from 'react-icons/md';
+import { MdOutlineSmsFailed, MdOutlineTextsms } from "react-icons/md";
 import { Routes, Route } from "react-router-dom";
 
-
 export default function Form() {
-    const [login, setLogin] = useState('');
-    const [password, setPassword] = useState('');
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [valid, setValid] = useState(false);
+  const [valid, setValid] = useState(false);
 
-    const handleLoginChange = (event) => {
-      setLogin(event.target.value);
-      setValid(login !== '' && password !== '');
-    };
-    const handlePasswordChange = (event) => {
-      setPassword(event.target.value);
-      setValid(login !== '' && password !== '');
-    };
-
+  const handleLoginChange = (event) => {
+    setLogin(event.target.value);
+    setValid(login !== "" && password !== "");
+  };
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+    setValid(login !== "" && password !== "");
+  };
 
   return (
-    <form className='form'>
-        <div className="form__content">
-            <h1>Admin</h1>
-            <div className="form__box">
-                <label>Login</label>
-                <input type="text" required=""
-                name='Login'
-                value={login}
-                onChange={handleLoginChange}
-                />
-            
-            </div>
-
-            <div className="form__box">
-                <label>Password</label>
-                <input type="password" required=""
-                name='Password'
-                value={password}
-                onChange={handlePasswordChange}
-                />
-            </div>
-
-            <div className='result-box'>
-                <button type='submit' disabled={!valid}>
-                Send
-                </button>
-                <div className='check-icons'>
-                {valid ? (
-                    <MdOutlineTextsms className='check-icon check-icon__sent green' />
-                ) : (
-                    <MdOutlineSmsFailed className='check-icon check-icon__error' />
-                )}
-                </div>
-            </div>
+    <form className="form">
+      <div className="form__content">
+        <h1>Admin</h1>
+        <div className="form__box">
+          <label>Login</label>
+          <input
+            type="text"
+            required=""
+            name="Login"
+            value={login}
+            onChange={handleLoginChange}
+          />
         </div>
+
+        <div className="form__box">
+          <label>Password</label>
+          <input
+            type="password"
+            required=""
+            name="Password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+
+        <div className="result-box">
+          <button type="submit" disabled={!valid}>
+            Send
+          </button>
+          <div className="check-icons">
+            {valid ? (
+              <MdOutlineTextsms className="check-icon check-icon__sent green" />
+            ) : (
+              <MdOutlineSmsFailed className="check-icon check-icon__error" />
+            )}
+          </div>
+        </div>
+      </div>
     </form>
-  )
+  );
 }
--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -1,88 +1,91 @@
-import React,{useState} from 'react';
-import {MdOutlineSmsFailed, MdOutlineTextsms} from 'react-icons/md';
-import Dashboard from './Dashboard';
-import { Link, Router, Route, Switch, Routes, useNavigate } from "react-router-dom";
-
+import React, { useState } from "react";
+import { MdOutlineSmsFailed, MdOutlineTextsms } from "react-icons/md";
+import Dashboard from "./Dashboard";
+import {
+  Link,
+  Router,
+  Route,
+  Switch,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
 
 const Admin = () => {
-    const [login, setLogin] = useState('');
-    const [password, setPassword] = useState('');
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [valid, setValid] = useState(false);
-    
-    const navigate = useNavigate();
+  const [valid, setValid] = useState(false);
 
-    const handleLoginChange = (event) => {
-      setLogin(event.target.value);
-      setValid(login === "admin" && password === 'admin');
-    };
-    const handlePasswordChange = (event) => {
-      setPassword(event.target.value);
-      setValid(login === "admin" && password === 'admin');
-    };
+  const navigate = useNavigate();
 
-    
+  const handleLoginChange = (event) => {
+    setLogin(event.target.value);
+    setValid(login === "admin" && password === "admin");
+  };
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+    setValid(login === "admin" && password === "admin");
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (valid) {
       // Redirect to the Dashboard page if login is successful
-      navigate('/dashboard');
+      navigate("/dashboard");
     }
   };
 
-
-    return (
-      <div className="admin">
+  return (
+    <div className="admin">
       <div className="Link">
         <Link className="Link__text" to="/">
           Ticket
         </Link>
       </div>
 
-      <div className="admin__content"> 
-        <form className='form' onSubmit={handleSubmit}>
+      <div className="admin__content">
+        <form className="form" onSubmit={handleSubmit}>
           <div className="form__content">
-              <h1>Admin</h1>
-              <div className="form__box">
-                  <label>Login</label>
-                  <input type="text" required=""
-                  name='Login'
-                  value={login}
-                  onChange={handleLoginChange}
-                  />
-              
-              </div>
+            <h1>Admin</h1>
+            <div className="form__box">
+              <label>Login</label>
+              <input
+                type="text"
+                required=""
+                name="Login"
+                value={login}
+                onChange={handleLoginChange}
+              />
+            </div>
 
-              <div className="form__box">
-                  <label>Password</label>
-                  <input type="password" required=""
-                  name='Password'
-                  value={password}
-                  onChange={handlePasswordChange}
-                  />
-              </div>
+            <div className="form__box">
+              <label>Password</label>
+              <input
+                type="password"
+                required=""
+                name="Password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+            </div>
 
-              <div className='result-box'>
-                  <button 
-                 
-                  type='submit' disabled={!valid}>
-                  Send
-                  </button>
-                  <div className='check-icons'>
-                  {valid ? (
-                      <MdOutlineTextsms className='check-icon check-icon__sent green' />
-                  ) : (
-                      <MdOutlineSmsFailed className='check-icon check-icon__error' />
-                  )}
-                  </div>
+            <div className="result-box">
+              <button type="submit" disabled={!valid}>
+                Send
+              </button>
+              <div className="check-icons">
+                {valid ? (
+                  <MdOutlineTextsms className="check-icon check-icon__sent green" />
+                ) : (
+                  <MdOutlineSmsFailed className="check-icon check-icon__error" />
+                )}
               </div>
+            </div>
           </div>
-        </form>          
+        </form>
       </div>
-
     </div>
-    )
-}
-export default Admin
-
+  );
+};
+export default Admin;
